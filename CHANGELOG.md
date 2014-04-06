6f29e74fa302f7d399d9f8805abbb5d3741e55d9
Vincent Talbot <vincent.talbot@gmail.com>
2014-04-06 07:28:17 -0400

    Version bump.

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+hhvm3 Cookbook
+==============
+
+#### 0.1.1
+- Make php replacement run once
+- Make installation of fastcgi run once
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email         'vincent.talbot@gmail.com'
 license                  'MIT'
 description              'Installs/Configures hhvm'
 long_description         IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version                  '0.1.0'
+version                  '0.1.1'
 
 depends                  'apt'
 
