9cf066a445d280fa9a9a761c28d2f689d67a3062
Vincent Talbot <vincent.talbot@gmail.com>
2014-04-06 07:28:01 -0400

    Make php replacement and installation of fastcgi run once.

--- recipes/package.rb
+++ recipes/package.rb
@@ -36,14 +36,37 @@ end
 
 package 'hhvm'
 
+php_replaced = "#{Chef::Config[:file_cache_path]}/php_replaced"
+hhvm_fastcgi_installed = "#{Chef::Config[:file_cache_path]}/hhvm_fastcgi_installed"
+
 if node['hhvm3']['replace_php']
   execute 'replace php' do
     command '/usr/bin/update-alternatives --install /usr/bin/php php /usr/bin/hhvm 60'
+    not_if do
+      ::File.exists?(php_replaced)
+    end
+  end
+
+  file php_replaced do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create_if_missing
   end
 end
 
 if node['hhvm3']['install_fastcgi']
   execute 'install fastcgi' do
     command '/usr/share/hhvm/install_fastcgi.sh'
+    not_if do
+      ::File.exists?(hhvm_fastcgi_installed)
+    end
+  end
+
+  file hhvm_fastcgi_installed do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create_if_missing
   end
 end
